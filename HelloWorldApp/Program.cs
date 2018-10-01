e5dbd9a659e5324dd908910f956f6fb366bc6df6
Folleach <folleach@yandex.ru>
2018-10-01 18:29:40 +0500

    Added write line

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello this big world!");
+            Console.WriteLine("Again.");
         }
     }
 }
